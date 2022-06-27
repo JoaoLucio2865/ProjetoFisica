4deba4e05bbf2cd29fb3584256bbb36a5d4dc68d
JoaoLucio2865 <105684589+JoaoLucio2865@users.noreply.github.com>
2022-06-27 19:24:03 -0300

    Update Fisica.cpp

--- Fisica.cpp
+++ Fisica.cpp
@@ -1,4 +1,4 @@
-//create by Erik Abdala, Jo„o Lucio, Juan Ozeas, Vinicius Lima
+//create by Erik Abdala, Jo√£o Lucio, Juan Ozeas, Vinicius Lima
 
 #include <bits/stdc++.h>
 #include <locale.h>
@@ -15,47 +15,47 @@ int main () {
     	
     	inicio:
         
-        printf("=========================== Calculadora b·sica para motores ===========================\n");
+        printf("=========================== Calculadora b√°sica para motores ===========================\n");
         printf("\nEscolha umas das funcionalidades abaixo:\n");
-        printf("\n1 - C·lculo da variaÁao da corrente elÈtrica\n");
-        printf("\n2 - C·lculo da energia armazenada\n");
-        printf("\n3 - C·lculo da potÍncia\n");
-        printf("\n4 - C·lculo da rotaÁ„o\n");
+        printf("\n1 - C√°lculo da varia√ßao da corrente el√©trica\n");
+        printf("\n2 - C√°lculo da energia armazenada\n");
+        printf("\n3 - C√°lculo da pot√™ncia\n");
+        printf("\n4 - C√°lculo da rota√ß√£o\n");
         printf("\n=======================================================================================\n");
         
         printf("\n> "); scanf("%d", &x);
         
         switch(x) {
             
-            case 1: printf("\nInsira o valor da tens„o induzida, em V (Volts):\n\n(Modelo de entrada: Valor real) (Ex: 12.0; 6.0)\n\n> U = "); scanf("%lf", &U); printf("\nInsira o valor da indut‚ncia, em H (Henrys):\n\n(Modelo de entrada: Valor real) (Ex: 10.0; 5.0)\n\n> L = "); scanf("%lf", &L);
+            case 1: printf("\nInsira o valor da tens√£o induzida, em V (Volts):\n\n(Modelo de entrada: Valor real) (Ex: 12.0; 6.0)\n\n> U = "); scanf("%lf", &U); printf("\nInsira o valor da indut√¢ncia, em H (Henrys):\n\n(Modelo de entrada: Valor real) (Ex: 10.0; 5.0)\n\n> L = "); scanf("%lf", &L);
             
-            printf("\nInsira o valor da variaÁ„o do tempo, em s (segundos):\n\n(Modelo de entrada: Valor real) (Ex: 1.0; 0.1)\n\n> ?t = "); scanf("%lf", &T);
+            printf("\nInsira o valor da varia√ß√£o do tempo, em s (segundos):\n\n(Modelo de entrada: Valor real) (Ex: 1.0; 0.1)\n\n> Dt = "); scanf("%lf", &T);
             
-            I = T * U / L; printf("\n?i = ( %.2lf s x %.2lf V ) / %.2lf H = %.2lf A   ?i = %.2lf A\n", T, U, L, I, I); break;
+            I = T * U / L; printf("\nDi = ( %.2lf s x %.2lf V ) / %.2lf H = %.2lf A\nDi = %.2lf A\n", T, U, L, I, I); break;
             
-            case 2: printf("\nInsira o valor da indut‚ncia, em H (Henrys):\n\n(Modelo de entrada: Valor real) (Ex: 10.0; 5.0)\n\n> L = "); scanf("%lf", &L); printf("\nInsira o valor da corrente elÈtrica induzida, em A (Amperes):\n\n(Modelo de entrada: Valor real) (Ex: 5.0; 2.5)\n\n> i = "); scanf("%lf", &I);
+            case 2: printf("\nInsira o valor da indut√¢ncia, em H (Henrys):\n\n(Modelo de entrada: Valor real) (Ex: 10.0; 5.0)\n\n> L = "); scanf("%lf", &L); printf("\nInsira o valor da corrente el√©trica induzida, em A (Amperes):\n\n(Modelo de entrada: Valor real) (Ex: 5.0; 2.5)\n\n> i = "); scanf("%lf", &I);
             
-            E = 1.0/2 * L * pow(I, 2); printf("\nE = ( 1/2 ) x %.2lf H x ( %.2lf A )^2 = %.2lf J   E = %.2lf J\n", L, I, E, E); break;
+            E = 1.0/2 * L * pow(I, 2); printf("\nE = ( 1/2 ) x %.2lf H x ( %.2lf A )^2 = %.2lf J\nE = %.2lf J\n", L, I, E, E); break;
             
-            case 3: printf("\nInsira o valor da tens„o induzida, em V (Volts):\n\n(Modelo de entrada: Valor real) (Ex: 12.0; 6.0)\n\n> U = "); scanf("%lf", &U); printf("\nInsira o valor da corrente elÈtrica induzida, em A (Amperes):\n\n(Modelo de entrada: Valor real) (Ex: 5.0; 2.5)\n\n> i = "); scanf("%lf", &I);
+            case 3: printf("\nInsira o valor da tens√£o induzida, em V (Volts):\n\n(Modelo de entrada: Valor real) (Ex: 12.0; 6.0)\n\n> U = "); scanf("%lf", &U); printf("\nInsira o valor da corrente el√©trica induzida, em A (Amperes):\n\n(Modelo de entrada: Valor real) (Ex: 5.0; 2.5)\n\n> i = "); scanf("%lf", &I);
             
             P = U * I; printf("\nP = %.2lf V x %.2lf A = %.2lf W   P = %.2lf W\n", U, I, P, P); break;
             
-            case 4: printf("\nInsira o valor da tens„o induzida, em V (Volts):\n\n(Modelo de entrada: Valor real) (Ex: 12.0; 6.0)\n\n> U = "); scanf("%lf", &U);
+            case 4: printf("\nInsira o valor da tens√£o induzida, em V (Volts):\n\n(Modelo de entrada: Valor real) (Ex: 12.0; 6.0)\n\n> U = "); scanf("%lf", &U);
             
-            printf("\nInsira o valor do n˙mero de espiras:\n\n(Modelo de entrada: Valor inteiro) (Ex: 2; 6)\n\n> n = "); scanf("%d", &n); printf("\nInsira o valor do fluxo magnÈtico, em Wb (Webers):\n\n(Modelo de entrada: Valor real) (Ex: 0.5; 0.05)\n\n> F = "); scanf("%lf", &M);
+            printf("\nInsira o valor do n√∫mero de espiras:\n\n(Modelo de entrada: Valor inteiro) (Ex: 2; 6)\n\n> n = "); scanf("%d", &n); printf("\nInsira o valor do fluxo magn√©tico, em Wb (Webers):\n\n(Modelo de entrada: Valor real) (Ex: 0.5; 0.05)\n\n> F = "); scanf("%lf", &M);
             
-            VA[0] = U * 3.14 / (2 * n * M); printf("\n? = (( %.2lf V x 3.14 ) / ( 2 x %d x %.2lf W )) = %.2lf rad/s\n", U, n, M, VA[0]);
+            VA[0] = U * 3.14 / (2 * n * M); printf("\nVA = (( %.2lf V x 3.14 ) / ( 2 x %d x %.2lf W )) = %.2lf rad/s\n", U, n, M, VA[0]);
             
-            VA[1] = VA[0] * 60 / ( 2 * 3.14 ); printf("\n? = (( %.2lf rad/s x 60 ) / ( 2 x 3.14 )) = %.2lf rpm\n", VA[0], VA[1]);
+            VA[1] = VA[0] * 60 / ( 2 * 3.14 ); printf("\VA = (( %.2lf rad/s x 60 ) / ( 2 x 3.14 )) = %.2lf rpm\n", VA[0], VA[1]);
             
             VA[2] = VA[0] / (2 * 3.14); printf("\nf = %.2lf rad/s / ( 2 * 3.14 ) = %.2lf Hz\n", VA[0], VA[2]); 
             
-            printf("\n? = %.2lf rad/s   ? = %.2lf rpm   f = %.2lf Hz\n", VA[0], VA[1], VA[2]); break;
+            printf("\nVA = %.2lf rad/s   VA = %.2lf rpm   f = %.2lf Hz\n", VA[0], VA[1], VA[2]); break;
             
             default:
             	
-            	printf("Por favor, insira uma opÁ„o valida!\n\n\n");
+            	printf("Por favor, insira uma op√ß√£o valida!\n\n\n");
             	system("pause");
             	system("cls");
 				goto inicio;
@@ -65,11 +65,11 @@ int main () {
         
         while(1) {
             
-            printf("\nAinda h· c·lculos a serem feitos? (S/N)\n\n> "); scanf("%s", res); printf("\n");
+            printf("\nAinda h√° c√°lculos a serem feitos? (S/N)\n\n> "); scanf("%s", res); printf("\n");
             
             if ((strcmp(res, "S") == 0)||(strcmp(res, "s") == 0)||(strcmp(res, "SIM") == 0)||(strcmp(res, "Sim") == 0)) { system("cls"); y = 0; break; }
             
-            else if ((strcmp(res, "N") == 0)||(strcmp(res, "n") == 0)||(strcmp(res, "N√O") == 0)||(strcmp(res, "N„o") == 0)) { y = 1; break; }
+            else if ((strcmp(res, "N") == 0)||(strcmp(res, "n") == 0)||(strcmp(res, "N√ÉO") == 0)||(strcmp(res, "N√£o") == 0)) { y = 1; break; }
             
             printf("\n");
             
